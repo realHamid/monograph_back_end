b6cd969afe1c02fad014d382a69b44fb29279ff4
realHamid <1122hamidjan@gmail.com>
2022-03-29 23:26:32 +0430

    creat distract model and migration

--- app/districts.php
+++ app/districts.php
@@ -5,28 +5,25 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class provinces extends Model
+class districts extends Model
 {
     //
-
     use SoftDeletes;
 
 
-//    region props
-    protected $fillable = ['name','img','date','note'];
-//    end region props
 
+//    region props
+    protected $fillable =['province_id','name','date','note','img'];
+
+    //    end region props
 
 
 //   region relation
 
     public function districts(){
-        return $this->hasMany(districts::class,'province_id','id');
+        return $this->belongsTo(provinces::class);
     }
 
 //   end region relation
-
-
-
 
 }
---
+++ database/migrations/2022_03_29_184802_create_districts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDistrictsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('districts', function (Blueprint $table) {
+
+            $table->bigIncrements('id');
+            $table->integer('province_id');
+            $table->string('name');
+            $table->date('date')->nullable();
+            $table->text('note')->nullable();
+            $table->string('img',350)->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+
+
+            $table->foreign('province_id')->references('id')->on('provinces');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('districts');
+    }
+}
