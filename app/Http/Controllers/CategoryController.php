0a19bbca915f3a0cd6ba57e5c6d1a5f9678dbf35
realHamid <1122hamidjan@gmail.com>
2022-03-31 10:43:29 +0430

    work on list category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -23,4 +23,15 @@ class CategoryController extends Controller
         }
     }
 
+    public function view (){
+
+        $category = categories::orderBy('id','desc')->get();
+
+        foreach ($category as $key =>  $row){
+            unset($row['created_at'],$row['updated_at'],$row['deleted_at']);
+        }
+
+        return json_encode($category);
+    }
+
 }
